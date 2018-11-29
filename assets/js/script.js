72f92f603d6b0f15f7c388fe555c065ef61364f6
Tim Taelman <tim.taelman@student.howest.be>
2018-11-29 09:44:38 +0100

    some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-document.write("test");
+let pressedKeys = [];
+
+function postKeyPad(){
+    
+    var body = JSON.stringify({
+          "sequence" : "1,2,3,4,5"
+        });
+    
+    
+    fetch("http://involved-htf-js-2018-prod.azurewebsites.net/api/challenge/1", 
+      {
+        method: "POST",
+        headers:{
+            "Content-Type":  "application/json",
+            "Accept": "application/json",
+            "x-team" : "test",
+            
+        },
+        body: body
+      })
+    .then(function(response) {
+    return response;
+  }).then(data => console.log(data));
+}
+postKeyPad();
+
+function addInputToArray(){
+    
+}
+
+
+$(".btnKeyPad").on("click", () => {
+    console.log(this.value);
+})
+
